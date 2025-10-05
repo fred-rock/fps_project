b25c7f0f768495ab06555714ee6589ea64861d4a
Adam Frederick <adam.paul.frederick@gmail.com>
2025-10-05 14:07:51 -0400

    attempting to merge changes that couldn't be merged for some reason

--- assets/scripts/player/player_controller.gd
+++ assets/scripts/player/player_controller.gd
@@ -3,6 +3,7 @@ class_name PlayerController extends CharacterBody3D
 @export var debug : bool = false
 @export_category("References")
 @export var camera : CameraController
+@export var camera_effects : CameraEffects
 @export var state_chart : StateChart
 @export var standing_collision : CollisionShape3D
 @export var crouching_collision : CollisionShape3D
@@ -18,26 +19,28 @@ class_name PlayerController extends CharacterBody3D
 @export var crouch_speed : float = -5.0
 @export_category("Jump Settings")
 @export var jump_velocity : float = 5.0
+@export var fall_velocity_threshold : float = -5.0
 
 var _input_dir : Vector2 = Vector2.ZERO
 var _movement_velocity : Vector3 = Vector3.ZERO
-var sprint_modifier : float = 0.0
-var crouch_modifier : float = 0.0
-var speed : float = 0.0
+var _sprint_modifier : float = 0.0
+var _crouch_modifier : float = 0.0
+var _speed : float = 0.0
+var current_fall_velocity : float
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		
-	var speed_modifier = sprint_modifier
-	speed = default_speed + speed_modifier
+	var speed_modifier = _sprint_modifier
+	_speed = default_speed + speed_modifier
 		
 	_input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var current_velocity = Vector2(_movement_velocity.x, _movement_velocity.z)
 	var direction = (transform.basis * Vector3(_input_dir.x, 0.0, _input_dir.y)).normalized()
 		
 	if direction:
-		current_velocity = lerp(current_velocity, Vector2(direction.x, direction.z) * speed, acceleration)
+		current_velocity = lerp(current_velocity, Vector2(direction.x, direction.z) * _speed, acceleration)
 	else: 
 		current_velocity = current_velocity.move_toward(Vector2.ZERO, deceleration)
 	
@@ -51,20 +54,28 @@ func update_rotation(rotation_input) -> void:
 	global_transform.basis = Basis.from_euler(rotation_input)
 	
 func sprint() -> void:
-	sprint_modifier = sprint_speed
+	_sprint_modifier = sprint_speed
 	
 func walk() -> void:
-	sprint_modifier = 0.0
+	_sprint_modifier = 0.0
 	
 func stand() -> void:
-	crouch_modifier = 0.0
+	_crouch_modifier = 0.0
 	standing_collision.disabled = false
 	crouching_collision.disabled = true
 	
 func crouch() -> void:
-	crouch_modifier = crouch_speed
+	_crouch_modifier = crouch_speed
 	standing_collision.disabled = true
 	crouching_collision.disabled = false
 	
 func jump() -> void:
 	velocity.y += jump_velocity
+
+func check_fall_speed() -> bool:
+	if current_fall_velocity < fall_velocity_threshold:
+		current_fall_velocity = 0.0
+		return true
+	else:
+		current_fall_velocity = 0.0
+		return false
--- scenes/player/camera_effects.gd
+++ scenes/player/camera_effects.gd
@@ -6,6 +6,10 @@ class_name CameraEffects extends Camera3D
 @export_category("Effects")
 @export var enable_tilt : bool = true
 @export var enable_fall_kick : bool = true
+@export var enable_damage_kick : bool = true
+@export var enable_weapon_kick : bool = true
+@export var enable_screen_shake : bool = true
+@export var enable_headbob : bool = true
 
 @export_category("Kick & Recoil Settings")
 @export_group("Run Tilt")
@@ -16,23 +20,55 @@ class_name CameraEffects extends Camera3D
 @export_group("Camera Kick")
 @export_subgroup("Fall Kick")
 @export var fall_time : float = 0.3
+@export_subgroup("Damage Kick")
+@export var damage_time : float = 0.3
+@export_subgroup("Weapon Kick")
+@export var weapon_decay : float = 0.5
+@export_subgroup("Headbob")
+@export_range(0.0, 0.1, 0.001) var bob_pitch : float = 0.05
+@export_range(0.0, 0.1, 0.001) var bob_roll : float = 0.025
+@export_range(0.0, 0.1, 0.001) var bob_up : float = 0.005
+@export_range(0.0, 0.1, 0.001) var bob_frequency : float = 6.0
 
 var _fall_value : float = 0.0
 var _fall_timer : float = 0.0
+
+var _damage_pitch : float = 0.0
+var _damage_roll : float = 0.0
+var _damage_timer : float = 0.0
+
+var _weapon_kick_angles : Vector3 = Vector3.ZERO
+
+var _screen_shake_tween : Tween
+
+var _step_timer : float = 0.0
+
+const MIN_SCREEN_SHAKE : float = 0.05
+const MAX_SCREEN_SHAKE : float = 0.5
 
 func _process(delta: float) -> void:
 	calculate_view_offset(delta)
 	
 	if Input.is_action_just_pressed("test"):
-		add_fall_kick(2.0)
+		add_screen_shake(5, 2)
 	
 func calculate_view_offset(delta):
 	if not player:
 		return
 		
 	_fall_timer -= delta
+	_damage_timer -= delta
 		
 	var velocity = player.velocity
+	
+	# Headbob Step Timer and Sin Value
+	var speed = Vector2(velocity.x, velocity.z).length()
+	if speed > 0.1 and player.is_on_floor():
+		_step_timer += delta * (speed / bob_frequency)
+		_step_timer = fmod(_step_timer, 1.0)
+	else:
+		_step_timer = 0.0
+	var bob_sin = sin(_step_timer * 2.0 * PI) * 0.5 # 0.5 reduces the magnitude of the sine wave, i.e. less movement
 	
 	var angles = Vector3.ZERO
 	var offset = Vector3.ZERO
@@ -57,9 +93,60 @@ func calculate_view_offset(delta):
 		angles.x -= fall_kick_amount
 		offset.y -= fall_kick_amount
 		
+	# Damage Kick
+	if enable_damage_kick:
+		var damage_ratio = max(0.0, _damage_timer / damage_time)
+		# damage_ratio = ease(damage_ratio, -2) # If you want to ease over time
+		angles.x += damage_ratio * _damage_pitch
+		angles.z += damage_ratio * _damage_roll
+		
+	# Weapon Kick
+	if enable_weapon_kick:
+		_weapon_kick_angles = _weapon_kick_angles.move_toward(Vector3.ZERO, weapon_decay * delta)
+		angles += _weapon_kick_angles
+		
+	# Headbob
+	if enable_headbob:
+		var pitch_delta = bob_sin * deg_to_rad(bob_pitch) * speed
+		angles.x -= pitch_delta
+		
+		var roll_delta = bob_sin * deg_to_rad(bob_roll) * speed
+		angles.z -= roll_delta
+		
+		var bob_height = bob_sin * speed * bob_up
+		offset.y += bob_height
+		
 	position = offset
 	rotation = angles
 	
-func add_fall_kick(fall_strength : float):
+func add_fall_kick(fall_strength: float):
 	_fall_value = deg_to_rad(fall_strength)
 	_fall_timer = fall_time
+	
+func add_damage_kick(pitch: float, roll: float, source: Vector3):
+	var forward = global_transform.basis.z
+	var right = global_transform.basis.x
+	var direction = global_position.direction_to(source)
+	var forward_dot = direction.dot(forward)
+	var right_dot = direction.dot(right)
+	_damage_pitch = deg_to_rad(pitch) * forward_dot
+	_damage_roll = deg_to_rad(roll) * right_dot
+	_damage_timer = damage_time
+	
+func add_weapon_kick(pitch: float, yaw: float, roll: float):
+	_weapon_kick_angles.x += deg_to_rad(pitch)
+	_weapon_kick_angles.y += deg_to_rad(randf_range(-yaw, yaw))
+	_weapon_kick_angles.z += deg_to_rad(randf_range(-roll, roll))
+	
+func add_screen_shake(amount: float, seconds: float) -> void:
+	if _screen_shake_tween:
+		_screen_shake_tween.kill()
+		
+	_screen_shake_tween = create_tween()
+	_screen_shake_tween.tween_method(update_screen_shake.bind(amount), 0.0, 1.0, seconds).set_ease(Tween.EASE_OUT)
+	
+func update_screen_shake(alpha: float, amount: float) -> void:
+	amount = remap(amount, 0.0, 1.0, MIN_SCREEN_SHAKE, MAX_SCREEN_SHAKE)
+	var current_shake_amount = amount * (1.0 - alpha)
+	h_offset = randf_range(-current_shake_amount, current_shake_amount)
+	v_offset = randf_range(-current_shake_amount, current_shake_amount)
