3f31d9a61e9129ec2f86d3a4656a5dcb0656b113
Adam Frederick <adam.paul.frederick@gmail.com>
2025-10-05 13:47:25 -0400

    added more kicks and screen shake

--- assets/scripts/player/states/grounded_state.gd
+++ assets/scripts/player/states/grounded_state.gd
@@ -1,7 +1,7 @@
 extends PlayerState
 
 func _on_grounded_state_physics_processing(_delta: float) -> void:
-	if Input.is_action_pressed("jump") and player_controller.is_on_floor():
+	if Input.is_action_just_pressed("jump") and player_controller.is_on_floor():
 		player_controller.jump()
 		player_controller.state_chart.send_event("onAirborne")
 		
