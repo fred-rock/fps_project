f4e9f240f7e647df96a8213c89b5b5af975ba085
Adam Frederick <adam.paul.frederick@gmail.com>
2025-10-12 15:31:36 -0400

    added step climbing

--- assets/scripts/player/components/camera_controller.gd
+++ assets/scripts/player/components/camera_controller.gd
@@ -10,14 +10,32 @@ class_name CameraController extends Node3D
 @export_category("Crouch Vertical Movement")
 @export var crouch_offset : float = 0.0
 @export var crouch_speed : float = 3.0
-
+@export_group("Step Smoothing")
+@export var step_speed : float = 8.0
 
 var _rotation : Vector3
+var _target_height : float
+var _step_smoothing : bool = false
+
+var offset_height : float
 
 const DEFAULT_HEIGHT : float = 0.5
 
-func _process(_delta: float) -> void:
-	update_camera_rotation(component_mouse_capture._mouse_input)
+func _ready() -> void:
+	_rotation = player_controller.rotation
+	offset_height = DEFAULT_HEIGHT
+
+func _process(delta: float) -> void:
+	update_camera_rotation(component_mouse_capture.mouse_input)
+	
+	if _step_smoothing: #TODO: Why is this not true??
+		_target_height = lerp(_target_height, 0.0, step_speed * delta)
+		if abs(_target_height) < 0.01:
+			_target_height = 0.0
+			_step_smoothing = false
+			
+		position.y = offset_height + _target_height
+		print("Position.y: " + str(position.y))
 
 func update_camera_rotation(input: Vector2) -> void:
 	_rotation.x += input.y
@@ -33,5 +51,10 @@ func update_camera_rotation(input: Vector2) -> void:
 	rotation.z = 0.0
 	
 func update_camera_height(delta: float, direction: int) -> void:
-	if position.y >= crouch_offset and position.y <= DEFAULT_HEIGHT:
-		position.y = clampf(position.y + (crouch_speed * direction) * delta, crouch_offset, DEFAULT_HEIGHT)
+	if offset_height >= crouch_offset and offset_height <= DEFAULT_HEIGHT:
+		offset_height = clampf(offset_height + (crouch_speed * direction) * delta, crouch_offset, DEFAULT_HEIGHT)
+		#print("Offset height: " + str(offset_height))
+
+func smooth_step(height_change : float):
+	_target_height -= height_change
+	_step_smoothing = true
--- assets/scripts/player/components/mouse_capture.gd
+++ assets/scripts/player/components/mouse_capture.gd
@@ -6,18 +6,18 @@ class_name MouseCaptureComponent extends Node
 @export var mouse_sensitivity : float = 0.005
 
 var _capture_mouse : bool
-var _mouse_input : Vector2
+var mouse_input : Vector2
 
 func _unhandled_input(event: InputEvent) -> void:
 	_capture_mouse = event is InputEventMouseMotion and Input.mouse_mode == Input. MOUSE_MODE_CAPTURED
 	if _capture_mouse:
-		_mouse_input.x += -event.screen_relative.x * mouse_sensitivity
-		_mouse_input.y += -event.screen_relative.y * mouse_sensitivity
+		mouse_input.x += -event.screen_relative.x * mouse_sensitivity
+		mouse_input.y += -event.screen_relative.y * mouse_sensitivity
 	if debug:
-		print(_mouse_input)
+		print(mouse_input)
 		
 func _ready() -> void:
 	Input.mouse_mode = current_mouse_mode
 	
 func _process(_delta: float) -> void:
-	_mouse_input = Vector2.ZERO
+	mouse_input = Vector2.ZERO
